f4c10b922952a3ea4713c74588b20e2e179361ea
tarunmahi <tarunmahendra007@gmail.com>
2025-02-02 22:45:36 +0530

    added messages

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const http = require('http')
-const{readFileSync, createReadStream} =require('fs')
-const server = http.createServer((req,res)=>{
-    const data = createReadStream('./folder/third.txt','utf8')
-    data.on('data',()=>{
-        data.pipe(res)
-    })
-})
-
-server.listen(5000);
-console.log('node is listening on port 5000');
+const
